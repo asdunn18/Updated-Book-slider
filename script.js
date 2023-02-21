46e62fcddefd3eada1cc4824d418c6508e4f9956
Adam Dunn <asdunn18@gmail.com>
2023-02-21 12:58:11 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+	$(".carousel").slick({
+		slidesToShow: 7,
+		dots: false,
+		centerMode: false,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 4,
+					slidesToScroll: 4,
+					infinite: true,
+					dots: false,
+				},
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+				},
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				},
+			},
+		],
+	});
+});
